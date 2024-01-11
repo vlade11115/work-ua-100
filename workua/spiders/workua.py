ebc67a5f053e928e9be7cdc976acecc9e41eca54
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2024-01-11 21:02:42 +0200

    Update job listings in jobs.json
    The job listings in jobs.json have been expanded and updated. The modifications include changes in job titles, adding new fields such as salary and URL, and detailed job descriptions for each job. The update aims to provide more comprehensive and up-to-date information about the jobs.

--- workua/spiders/workua.py
+++ workua/spiders/workua.py
@@ -6,29 +6,27 @@ class WorkUaSpider(scrapy.Spider):
 
     start_urls = ["https://www.work.ua/jobs-dnipro/"]
 
+    def parse_vacancy(self, response):
+        title = response.xpath('//h1[@id="h1-name"]/text()').get()
+        salary = (
+            response.css(".glyphicon-hryvnia")[-1]
+            .xpath("./following-sibling::span/text()")
+            .get()
+            .replace("\u202f", "")
+            .replace("\xa0", "")
+            .replace("\u2009", "")
+        )
+        if "грн" not in salary:
+            salary = None
+
+        description = "".join(response.xpath('//div[@id="job-description"]').getall())
+        employer = response.css(".glyphicon-company").xpath("./following-sibling::a/span/text()").get()
+        yield {"url": response.url, "title": title, "salary": salary, "description": description, "employer": employer}
+
     def parse(self, response):
         for card in response.css(".card"):
-            salary = (
-                card.css(".add-bottom")
-                .xpath("./following-sibling::div/span/text()")
-                .get()
-                .replace("\u202f", "")
-                .replace("\xa0", "")
-                .replace("\u2009", "")
-            )
-            if "грн" not in salary:
-                salary = None
-            description = card.css(".cut-bottom::text").getall()
-            description = " ".join(description).strip()
-            if salary is None:
-                employer = card.css(".add-bottom").xpath("./following-sibling::div/span/span/text()").get()
-            else:
-                employer = (
-                    card.css(".add-bottom")
-                    .xpath("./following-sibling::div")
-                    .xpath("./following-sibling::div/span/span/text()")
-                    .get()
-                )
-            title = card.css(".add-bottom").xpath("./h2/a/@title").get()
-            item = {"title": title, "salary": salary, "description": description, "employer": employer}
-            yield item
+            vacancy_url = card.css(".add-bottom").xpath("./h2/a/@href").get()
+            if vacancy_url is None:
+                continue
+            yield response.follow(vacancy_url, callback=self.parse_vacancy)
+        # TODO: pagination
